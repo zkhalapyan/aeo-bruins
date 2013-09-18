b823f35204a4a3f2938b79039d676e7073d0cf96
Zorayr Khalapyan <zkhalapyan@gmail.com>
2013-09-17 21:52:37 -0700

    Created Rails Project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AeoBruins::Application.config.secret_key_base = 'c87fc84998eb11d473a44475d3737b7cc051af9b78dd211ab9af87ee74af05c0f9ecc8abe1d192e88290096e80bcf123b551639c555c564d4c047f942f499d5e'
